95c6fb366df9e9d1c2faf1ec9f90c87527e38e6a
Russley Shaw <russleyshaw@gmail.com>
2018-12-01 16:42:58 -0600

    Remove app model

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import DevTools from "mobx-react-devtools";
 import * as React from "react";
 import { render } from "react-dom";
 
-import { App } from "./App";
-import { AppModel } from "./app_model";
-
 import "./style/index.scss";
-
-const model = new AppModel();
 
 render(
     <>
